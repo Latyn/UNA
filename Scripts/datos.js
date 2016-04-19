4c8fb48a773bd5e22111bd21c3421ccbe7cdb9c0
Michael Gonzalez <magova08@yahoo.com>
2016-04-18 22:42:31 -0600

    Adding styles and fuctions

--- Scripts/datos.js
+++ Scripts/datos.js
@@ -2,6 +2,18 @@ function loadCarrito(){
   prods=retrieveCarritoFromString(datos);
   storeCarrito("Productos",prods);
 }
+
 function retrieveCarritoFromString(jsonString){
  return JSON.parse(jsonString,revive);
+}
+
+function definePrototype(){
+	var Book = {
+		
+		codigo : "Null" ,
+		description : "",
+		imageUrl : "Book/Cover/Default.jpg"
+		price : ""
+		
+	}
 }
