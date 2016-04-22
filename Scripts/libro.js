162c55f4f5e3ec01883b3e3f9be01a3f36cae406
Michael Gonzalez <magova08@yahoo.com>
2016-04-22 08:43:27 -0600

    adding total por libro

--- Scripts/libro.js
+++ Scripts/libro.js
@@ -1,6 +1,7 @@
 ﻿
   function Libro(codigo,name,descripcion,imageUrl,price,cantidad) {
-    this.Libro(codigo,name,descripcion,imageUrl,price,cantidad);
+	total = cantidad * price;
+    this.Libro(codigo,name,descripcion,imageUrl,price,cantidad, total);
   }
   
   // Se definen variables y funciones del prototipo
@@ -11,6 +12,7 @@
     imageUrl:"",
 	price:"",
 	cantidad:"",
+    total:"",
 	//categorias: [],
 	Libro: function(codigo,name,descripcion,imageUrl,price, cantidad){
 		this.codigo=codigo;
@@ -19,6 +21,7 @@
 		this.imageUrl=imageUrl;
 		this.precio=price;
 		this.cantidad=cantidad;
+		this.total= cantidad * price;
 	},
 	toString:function(){
 	  return this.codigo+" - "+this.descripcion;
@@ -26,7 +29,7 @@
   }
   
   Libro.from= function(plain){
-    libro = new Libro(plain.codigo,plain.name,plain.descripcion,plain.imageUrl,plain.price,plain.cantidad);
+    libro = new Libro(plain.codigo,plain.name,plain.descripcion,plain.imageUrl,plain.price,plain.cantidad,plain.precio);
 	return libro;
   }
   
@@ -38,7 +41,8 @@
 		descripcion : libro.descripcion,
 		imageUrl : libro.imageUrl,
 		price: libro.price,
-		cantidad: libro.cantidad
+		cantidad: libro.cantidad,
+		total : libro.total
     };	
   }
   
